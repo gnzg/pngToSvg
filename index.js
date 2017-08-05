9ea90b0e52bdaa863e2f7dd971762549714316bc
gnzg <gnzg@aol.com>
2017-08-05 16:20:45 +0200

    working alpha

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
 var pixel = require('pixel');
 var svg = require('pixel-to-svg');
 var fs = require('fs');
-var convertedSVG;
 var me = this;
-var prompt = require('prompt');
+var dir = require('node-dir');
 
-/* ask user to set name of origin file and name of new file once converted */
-prompt.start();
 
-prompt.message = "Convert what to SVG? Enter";
-prompt.delimiter = ' ';
-prompt.get(['fileName'], function (err = null, result) {
+var files = dir.files(__dirname, {sync:true, recursive:false});
+//console.log(files);
 
-    /* only perform conversion if a file type has been provided */
-    if (result.fileName.indexOf(".")>-1) {
 
-        pixel.parse('./' + result.fileName)
+// filters file array and returns an array of ONLY the png files
+var filteredFiles = files.filter(function(legitFile) {
+    return legitFile.match( /(png|jpg)/ );
+});
+// filter for actual file name, not file path
+filteredFiles.forEach(function (item, index) {
+    filteredFiles[index] = item.substring(item.lastIndexOf("/") + 1);
+});
+console.log("Found these guys: " + filteredFiles);
+
+filteredFiles.forEach(function(item, index ) {
+
+        pixel.parse('./' + item)
             .then(function (images) {
                 /*console.log(svg.convert(images[0]));*/
                 me.convertedSVG = svg.convert(images[0]);
-                console.log(me.convertedSVG);
+                //console.log(me.convertedSVG);
 
                 /* create new file name for the svg file*/
-                var newName = result.fileName;
-                newName = newName.replace(/\.png/g, '').replace(/\.jpg/g, '');
-                console.log(newName);
+                var newName = item;
+                newName = item.replace(/\.png/g, '').replace(/\.jpg/g, '');
+                console.log(item + " has been converted.");
 
                 fs.writeFile("./" + newName + ".svg", me.convertedSVG, function (err) {
                     if (err) {
                         return console.log(err);
                     }
-                    console.log("The file was saved!");
+
                 });
             });
-    } else {
-        console.log('Invalid input. Forgot the file extension?');
-    }
 });
